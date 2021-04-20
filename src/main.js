829ccde75e6eb0131fe52f96ae7a7d15d4cb912f
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-04-20 14:37:00 -0700

    update .js files with async/await
    Co-authored-by: Ahmed Ghouzlane <ahmedghouzlane@gmail.com>

--- src/main.js
+++ src/main.js
@@ -11,17 +11,40 @@ function clearFields() {
   $('.showTemp').text("");
 }
 
+// $(document).ready(function() {
+//   $('#weatherLocation').click(function() {
+//     let city = $('#location').val();
+//     clearFields();
+//     let promise = WeatherService.getWeather(city);
+//     promise.then(function(response) {
+//       const body = JSON.parse(response);
+//       $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+//       $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+//     }, function(error) {
+//       $('.showErrors').text(`There was an error processing your request: ${error}`);
+//     });
+//   });
+// });
+
+function getElements(response) {
+  if (response.main) {
+    $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
+    $('.showTemp').text(`The temperature in Fahrenheit is ${response.main.temp} degrees.`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response.message}`);
+  }
+}
+
+async function makeApiCall(city){
+  const response = await WeatherService.getWeather(city);
+  getElements(response);
+}
+
+
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     clearFields();
-    let promise = WeatherService.getWeather(city);
-    promise.then(function(response) {
-      const body = JSON.parse(response);
-      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error}`);
-    });
+    makeApiCall(city);
   });
 });
